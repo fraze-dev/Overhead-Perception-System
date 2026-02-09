c0cfee87a452be5a64eac228689eb0454d5c3271
fraze-dev <aaron.fraze3@gmail.com>
2026-02-09 11:01:52 -0500

    Add exploration scripts and library usage examples
    - RealSense API wrapper exploration
    - Library function examples and testing utilities
    - Development/debugging tools

---
+++ tests/lib_examples.py
@@ -0,0 +1,19 @@
+import pyrealsense2 as rs
+
+'''
+https://realsenseai.github.io/librealsense/python_docs/_generated/pyrealsense2.html
+
+test1 = rs.config()
+test2 = rs.stream()
+test3 = rs.pipeline()
+test4 = rs.align()
+test5 = rs.format()
+test6 = rs.any()
+test7 = rs.any_intel()
+test8 = rs.auto_calibrated_device()
+test9 = rs.BufData()
+test10 = rs.calib_target_type()
+test11 = rs.intrinsics()
+test12 = rs.firmware_logger()
+
+'''
---
+++ wrapper_explore.py
@@ -0,0 +1,13 @@
+import pyrealsense2 as rs
+
+"""
+Explorer for Python wrapper pyrealsense2
+Author: Aaron Fraze
+Date: February 7, 2026
+Purpose: Search through documentation of available Classes, methods and functions
+"""
+#help(rs)
+#help(rs.pipeline)
+#help(rs.depth_frame)
+help(rs.video_frame)
+#dir(rs)
